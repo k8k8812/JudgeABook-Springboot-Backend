28a07d82a87641d83fc1e0dea0a35285c78940ef
Sarah Willingham <sarah.willingham1000@gmail.com>
2022-05-16 20:17:26 -0500

    upload repositories and blank controller classes

---
+++ src/main/java/com/cognixia/jump/repository/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.cognixia.jump.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cognixia.jump.model.Review;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+}
