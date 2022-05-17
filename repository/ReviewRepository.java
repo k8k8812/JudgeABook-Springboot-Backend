025b61dee8d878fb3773bf0ff3a60d8c4e6bca1d
SarahWillingham <102173982+SarahWillingham@users.noreply.github.com>
2022-05-16 20:34:04 -0500

    Delete repository directory

--- repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognixia.jump.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognixia.jump.model.Review;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-
-}
--- repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognixia.jump.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cognixia.jump.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
