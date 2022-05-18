7f4ea46b5f7f18eaa3b4cc26d46fc6c569829315
sam-mcginnis <sammcg96@aol.com>
2022-05-18 16:04:42 -0400

    test push for AWS

--- src/main/java/com/cognixia/jump/controller/BookController.java
+++ src/main/java/com/cognixia/jump/controller/BookController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.cognixia.jump.exceptions.ResourceNotFoundException;
 import com.cognixia.jump.model.Book;
 import com.cognixia.jump.repository.BookRepository;
 
+@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
 @RequestMapping("/api")
 @RestController
 public class BookController {
--- src/main/java/com/cognixia/jump/controller/ReviewController.java
+++ src/main/java/com/cognixia/jump/controller/ReviewController.java
@@ -7,9 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.cognixia.jump.model.Book;
 import com.cognixia.jump.model.Review;
@@ -19,6 +22,9 @@ import com.cognixia.jump.repository.ReviewRepository;
 import com.cognixia.jump.repository.UserRepository;
 import com.cognixia.jump.util.JwtUtil;
 
+@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
+@RequestMapping("/api")
+@RestController
 public class ReviewController {
 	
 	@Autowired
